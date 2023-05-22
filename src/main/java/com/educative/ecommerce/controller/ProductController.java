214120b79271671bc1e6185acef7cd62cb8b84f2
Sebastian Graeber <sgraeber@ucsd.edu>
2023-05-21 20:08:00 -0700

    created list and update product APIs

--- src/main/java/com/educative/ecommerce/controller/ProductController.java
+++ src/main/java/com/educative/ecommerce/controller/ProductController.java
@@ -1,10 +1,13 @@
 package com.educative.ecommerce.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +18,8 @@ import com.educative.ecommerce.dto.product.ProductDto;
 import com.educative.ecommerce.model.Category;
 import com.educative.ecommerce.service.CategoryService;
 import com.educative.ecommerce.service.ProductService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/product")
@@ -36,6 +41,26 @@ public class ProductController {
     Category category = optionalCategory.get();
     productService.addProduct(productDto, category);
     return new ResponseEntity<ApiResponse>(new ApiResponse(true, "Product has been added"), HttpStatus.CREATED);
+  }
+
+  // list all the products
+  @GetMapping("/")
+  public ResponseEntity<List<ProductDto>> getProducts() {
+    List<ProductDto> productDtos = productService.listProducts();
+    return new ResponseEntity<List<ProductDto>>(productDtos, HttpStatus.OK);
+  }
+
+  // update a product
+  @GetMapping("/update/{productId}")
+  public ResponseEntity<ApiResponse> updateProduct(@PathVariable("productId") Integer productId,
+      @RequestBody @Valid ProductDto productDto) {
+    Optional<Category> optionalCategory = categoryService.readCategory(productDto.getCategoryId());
+    if (!optionalCategory.isPresent()) {
+      return new ResponseEntity<ApiResponse>(new ApiResponse(false, "category is invalid"), HttpStatus.CONFLICT);
+    }
+    Category category = optionalCategory.get();
+    productService.updateProduct(productId, productDto, category);
+    return new ResponseEntity<ApiResponse>(new ApiResponse(true, "Product has been updated"), HttpStatus.OK);
   }
 
 }
--- src/main/java/com/educative/ecommerce/service/ProductService.java
+++ src/main/java/com/educative/ecommerce/service/ProductService.java
@@ -1,5 +1,8 @@
 package com.educative.ecommerce.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -27,5 +30,26 @@ public class ProductService {
     product.setPrice(productDto.getPrice());
     product.setName(productDto.getName());
     return product;
+  }
+
+  public List<ProductDto> listProducts() {
+    // first fetch all the products
+    List<Product> products = productRepository.findAll();
+    List<ProductDto> productDtos = new ArrayList<>();
+
+    for (Product product : products) {
+      // for each product change it to DTO
+      productDtos.add(new ProductDto(product));
+    }
+
+    return productDtos;
+  }
+
+  public void updateProduct(Integer productId, ProductDto productDto, Category category) {
+    Product product = getProductFromDto(productDto, category);
+    // set the id for updating
+    product.setId(productId);
+    // update
+    productRepository.save(product);
   }
 }
