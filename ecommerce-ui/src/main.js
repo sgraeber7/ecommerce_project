ab87d5461f26e9c52cccf70470c47c0bd7f2340f
Sebastian Graeber <sgraeber@ucsd.edu>
2023-05-22 17:52:26 -0700

    ui  for adding and displaying categories

--- ecommerce-ui/src/main.js
+++ ecommerce-ui/src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import swal from 'sweetalert'
 
-createApp(App).mount('#app')
+window.Swal = swal
+window.axios = require('axios')
+createApp(App).use(router).mount('#app')
